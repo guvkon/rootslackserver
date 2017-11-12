f66ba29ef46c38ab409c45896bdff194a3c7d1ba
Konstantin Gusev <guvkon@gmail.com>
2017-11-13 02:13:45 +0700

    Change router entry point

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const express = require('express');
 const app = express();
 
 app.set('view engine', 'pug');
-app.use('/slack', require('../slackgameserver'));
+app.use('/slack/game', require('../slackgameserver'));
 
 app.listen(process.env.SERVER_PORT, function () {
     console.log('Listening on ', this.address());
