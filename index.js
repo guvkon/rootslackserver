d33db43467dab0841a285fa4d3eed6e3838373a8
Konstantin Gusev <guvkon@gmail.com>
2017-11-13 02:54:38 +0700

    Add test route

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const app = express();
 
 app.set('view engine', 'pug');
 app.use('/slack/game', require('../slackgameserver'));
+app.get('/slack/test', (req, res) => {
+    res.send("Test");
+});
 
 app.listen(process.env.SERVER_PORT, function () {
     console.log('Listening on ', this.address());
